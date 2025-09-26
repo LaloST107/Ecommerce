65de445a0c5efc7ed1791b19ff5d0e6150779af8
Danis40219 <galletadana000@gmail.com>
2025-09-26 15:25:21 -0600

    Creación de la  tabla tipo de clientes y tabla envios

--- Backend/shop/src/main/resources/db/migration/V12__TBL_CREATE_ENVIOS.sql
+++ Backend/shop/src/main/resources/db/migration/V12__TBL_CREATE_ENVIOS.sql
@@ -4,14 +4,11 @@ INCREMENT BY 1;
 
 CREATE TABLE TBL_ENVIO (
     idEnvio INT PRIMARY KEY DEFAULT nextval('SEQ_ID_ENVIO'),
-    orden_id INT NOT NULL,
 	costo_envío NUMERIC(10,2) NOT NULL,
 	fecha_envío date,
 	fecha_estimada_llegada date,
 	estado_envío VARCHAR(255),
 	proveedor_envío VARCHAR(255),
-	dirección_entrega VARCHAR(255),
-	FOREIGN KEY (orden_id) REFERENCES TBL_ORDEN(idOrden)
-	ON DELETE CASCADE
+	dirección_entrega VARCHAR(255)
 );
 
